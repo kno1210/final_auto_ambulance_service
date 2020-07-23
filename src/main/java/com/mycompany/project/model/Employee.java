94dc42f2f67b3fa5c188bcb66787b1febc96431e
kno1210 <kno975612@gmail.com>
2020-07-23 10:28:30 +0900

    db 연결

--- src/main/java/com/mycompany/project/model/Employee.java
+++ src/main/java/com/mycompany/project/model/Employee.java
@@ -4,4 +4,9 @@ public class Employee {
 	
 	String eno; // 직원번호 //20170303-1234567 이런식으로
 	String ename; // 직원 이름
+	String eage; // 직원 나이
+	String esex; // 직원 성별 //f or m
+	String etel; // 직원 전화번호
+	String eposition; // 직원 담당업무
+	String efaceid; // 직원 얼굴인식 아이디
 }
