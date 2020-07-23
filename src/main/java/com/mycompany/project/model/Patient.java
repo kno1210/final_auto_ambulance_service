8d78bcdf8c57e9ead1c99d1e64eedef87293b392
kno1210 <koh975612@gmail.com>
2020-07-23 10:56:39 +0900

    ㄱㄱ

--- src/main/java/com/mycompany/project/model/Patient.java
+++ src/main/java/com/mycompany/project/model/Patient.java
@@ -1,21 +1,15 @@
 package com.mycompany.project.model;
 
 public class Patient {
-	
-	// 이 클래스가 등록한 회원의 정보를 담는 클래스일 경우
-	//String pno; // 서비스 가입한 회원(환자)의 식별번호
-	//String pname; // 회원 이름
-	//String pphone; // 회원 휴대폰 번호
-	//String paddr; // 회원 집주소
-	//int page; // 회원 나이
-	//char psex; // 회원 성별
-	
+
 	// 이 클래스가 접수가 들어온 환자의 정보를 담는 클래스일 경우
-	String receptionTime; // 접수 시간 // sdf로 String으로 저장
-	String location; // 환자 위치
-	String symptom; // 증상
-	char sex; // 성별
-	int age; // 나이
-	String bloodType; // 혈액형
-	
+	int pno; // 환자 번호
+	String preportTime; // 신고 시간 // sdf로 String으로 저장
+	String preportTel; // 신고를 한 번호
+	String plocation; // 환자 위치
+	String pname; // 환자 이름 // 익명이라면 anonymous
+	String psymptom; // 증상
+	char psex; // 성별
+	int page; // 나이
+	String pbloodType; // 혈액형
 }
